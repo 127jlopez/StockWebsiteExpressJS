717599ab653cc08ff49a11b116b4375119256a16
Jose Lopez <>
2022-06-14 22:35:13 -0700

    Cleaned up Server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require("express");
-const _bodyParser = require("body-parser");
 
 const app = express();
-
+const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(_bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-  console.log("Listening on port: 3000");
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
 });
 
+// Home Page
 const homeRouter = require("./routes/home.js");
-app.use("/home", homeRouter);
+app.use("/", homeRouter);
